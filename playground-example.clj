837b18217244cefe702e88e8ce22fd495d5c4a5c
Andrew Panfilov <apanfilov@cogniance.com>
2013-09-30 17:35:24 +0300

    add cascalog example

---
+++ playground-example.clj
@@ -0,0 +1,6 @@
+hadoop-cascading-cascalog
+=========================
+
+* http://wiki.apache.org/hadoop/WordCount
+* https://github.com/Cascading/Impatient/blob/master/part2/src/main/java/impatient/Main.java
+* https://github.com/Quantisan/Impatient/blob/cascalog/part2/src/impatient/core.clj
